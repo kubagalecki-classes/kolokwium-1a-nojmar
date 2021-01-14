5cc495b02f0c064438b040d8eb1354433e0cf951
nojmar <73884817+nojmar@users.noreply.github.com>
2021-01-14 13:40:26 +0100

    Update zad2.hpp

--- include/zad2.hpp
+++ include/zad2.hpp
@@ -6,4 +6,22 @@
 
 // tutaj klasa Penne
 
+class Penne: public Makaron
+{
+  
+ double ileMaki(unsigned P) const{
+   return (double) P;
+} 
+
+
+};
 // tutaj definicja metody gotujMakaron
+static Makaron*  Makaron::gotujMakaron( const string& str ){
+ 
+  if  (str.front() == str.back()){
+    return new Tagliatelle(3.14, 0.42, 0.1);
+  }
+  else {
+    return new Penne();
+  }
+}
