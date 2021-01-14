88e42cbc7e471bcce0c02050c4068c37e5ef760d
nojmar <73884817+nojmar@users.noreply.github.com>
2021-01-14 13:36:55 +0100

    Update zad3.hpp

--- include/zad3.hpp
+++ include/zad3.hpp
@@ -2,4 +2,10 @@
 #include <cstdint>
 #include <functional>
 
+using namespace std;
 // tutaj funkcja polejSosem
+template<typename T>
+size_t polejSosem(const Tagliatelle & tagia, const T & obj) {
+
+    return obj.polej(tagia);
+}
