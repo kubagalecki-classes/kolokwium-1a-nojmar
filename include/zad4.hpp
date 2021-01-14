ee0035641bc9a1c46456f3d489dfb2ea849229c6
nojmar <73884817+nojmar@users.noreply.github.com>
2021-01-14 13:32:55 +0100

    Update zad4.hpp

--- include/zad4.hpp
+++ include/zad4.hpp
@@ -1,5 +1,23 @@
 #include "zad1.hpp"
 
 #include <vector>
-
+using namespace std;
 // tutaj funkcja obliczMake
+double obliczMake(vector <Tagliatelle*> tagl){
+
+  double M =0;
+
+  for (int i = 0; i>tagl.size(); i--){
+    M += tagl[tagl.size()-1].ileMaki(i+1);
+  }
+
+  if (M > 100){
+    throw (int)1;
+  }
+
+  if (M > 50 && M <= 100){
+    throw (double) 2.0;
+  }
+
+  return M;
+}
